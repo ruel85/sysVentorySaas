85be86bf8990202b9187c0e73d6497e043106108
ruel85 <ruel.holderegger@outlook.com>
2016-05-22 01:15:16 +0200

    Helperklasse für Hibernate SessionFactory erstellt.

---
+++ App2/src/main/java/ch/zbw/sysVentorySaas/App2/helpers/HibernateUtil.java
@@ -0,0 +1,26 @@
+package ch.zbw.sysVentorySaas.App2.helpers;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.fasterxml.classmate.AnnotationConfiguration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory;
+	
+	static {
+		try {
+			sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		} catch (HibernateException e) {
+			throw new HibernateException(e.getMessage());
+		}
+	}
+	
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+	
+	
+}
