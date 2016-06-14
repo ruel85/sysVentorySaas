6ff3f8be55af46bb61ce6392d73167de6027a752
ruel85 <ruel.holderegger@outlook.com>
2016-06-14 19:07:31 +0200

    -

---
+++ App2/src/test/java/ch/zbw/sysVentorySaas/Test/App/TestRestService.java
@@ -0,0 +1,15 @@
+package ch.zbw.sysVentorySaas.Test.App;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRestService {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
