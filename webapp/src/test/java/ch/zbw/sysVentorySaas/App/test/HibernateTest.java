6d41b16a1620b0341712e22c7c8ef53394faa417
ruel85 <ruel.holderegger@outlook.com>
2016-06-24 15:10:46 +0200

    HibernateTest optimiert

--- webapp/src/test/java/ch/zbw/sysVentorySaas/App/test/HibernateTest.java
+++ webapp/src/test/java/ch/zbw/sysVentorySaas/App/test/HibernateTest.java
@@ -89,93 +89,87 @@ public class HibernateTest {
 	@Test
 	public void TestSoftware_CRUD()
 	{
-		SoftwareDAO softwareDAO = new SoftwareDAO();
-		Software newSoftware = softwareDAO.createSoftware(new Software("NEST/IS-E", "InnoSolv AG", "2016.6"));
+		Software newSoftware = SoftwareDAO.createSoftware(new Software("NEST/IS-E", "InnoSolv AG", "2016.6"));
 		
 		assertEquals("InnoSolv AG", newSoftware.getManufacturer());
 		assertEquals("NEST/IS-E", newSoftware.getName());
 		assertEquals("2016.6", newSoftware.getVersion());
 		
-		Software softwareSelected = softwareDAO.getSoftwarebyId(newSoftware.getIdSoftware());
+		Software softwareSelected = SoftwareDAO.getSoftwarebyId(newSoftware.getIdSoftware());
 		assertEquals(newSoftware.getIdSoftware(), softwareSelected.getIdSoftware());
 		
-		softwareDAO.deleteSoftware(newSoftware);
-		assertNull(softwareDAO.getSoftwarebyId(newSoftware.getIdSoftware()));	
+		SoftwareDAO.deleteSoftware(newSoftware);
+		assertNull(SoftwareDAO.getSoftwarebyId(newSoftware.getIdSoftware()));	
 	}
 	
 	@Test
 	public void TestOperatingSystem_CRUD()
 	{
 		OperatingSystem op = new OperatingSystem("Windows 10", "Microsoft Windows", "64-bit");
-		OperatingSystemDAO opDAO = new OperatingSystemDAO();
-		opDAO.createOperatingSystem(op);
+		OperatingSystemDAO.createOperatingSystem(op);
 		
-		assertEquals("Windows 10", opDAO.getOperatingSystemById(1).getName());
-		assertEquals("Microsoft Windows", opDAO.getOperatingSystemById(1).getManufacturer());
-		assertEquals("64-bit", opDAO.getOperatingSystemById(1).getArchitecture());
+		assertEquals("Windows 10", OperatingSystemDAO.getOperatingSystemById(1).getName());
+		assertEquals("Microsoft Windows", OperatingSystemDAO.getOperatingSystemById(1).getManufacturer());
+		assertEquals("64-bit", OperatingSystemDAO.getOperatingSystemById(1).getArchitecture());
 		
-		opDAO.deleteOperatingSystem(opDAO.getOperatingSystemById(1));
-		assertNull(opDAO.getOperatingSystemById(1));		
+		OperatingSystemDAO.deleteOperatingSystem(OperatingSystemDAO.getOperatingSystemById(1));
+		assertNull(OperatingSystemDAO.getOperatingSystemById(1));		
 	}
 	
 	@Test
 	public void TestProcessor_CRUD()
 	{
 		Processor pr = new Processor("i7-465U", "Intel(R)",  4, "2.30 GHz");
-		ProcessorDAO prDAO = new ProcessorDAO();
-		prDAO.createProcessor(pr);
+		ProcessorDAO.createProcessor(pr);
 		
-		Processor prSelected = prDAO.getProcessorById(1);
+		Processor prSelected = ProcessorDAO.getProcessorById(1);
 		assertEquals("i7-465U", prSelected.getName());
 		assertEquals("Intel(R)", prSelected.getManufacturer());
 		assertEquals(4, prSelected.getCores());
 		assertEquals("2.30 GHz", prSelected.getFrequency());
 		
-		prDAO.deleteProcessor(prSelected);
-		assertNull(prDAO.getProcessorById(1));
+		ProcessorDAO.deleteProcessor(prSelected);
+		assertNull(ProcessorDAO.getProcessorById(1));
 	}
 	
 	@Test
 	public void TestPrinter_CRUD(){
 		Printer p = new Printer("Canon", "Farbdrucker Zimmer links");
-		PrinterDAO pDAO = new PrinterDAO();
-		pDAO.createPrinter(p);
+		PrinterDAO.createPrinter(p);
 		
-		Printer pSelected = pDAO.getPrinterById(1);
+		Printer pSelected = PrinterDAO.getPrinterById(1);
 		assertEquals("Canon", pSelected.getName());
 		assertEquals("Farbdrucker Zimmer links", pSelected.getDescription());
 		
-		pDAO.deleteProcessor(pSelected);
-		assertNull(pDAO.getPrinterById(1));
+		PrinterDAO.deleteProcessor(pSelected);
+		assertNull(PrinterDAO.getPrinterById(1));
 	}
 	
 	@Test
 	public void TestNetworkInterface_CRUD(){
 		NetworkInterface ni = new NetworkInterface("Marvell AVASTAR Wireless-AC Network Controller");
-		NetworkInterfaceDAO niDAO = new NetworkInterfaceDAO();
-		niDAO.createNetworkInterface(ni);
+		NetworkInterfaceDAO.createNetworkInterface(ni);
 		
-		NetworkInterface niSelected = niDAO.getNetworkInterfaceById(1);
+		NetworkInterface niSelected = NetworkInterfaceDAO.getNetworkInterfaceById(1);
 		assertEquals("Marvell AVASTAR Wireless-AC Network Controller", niSelected.getName());
 		
-		niDAO.deleteNetworkInterface(niSelected);
-		assertNull(niDAO.getNetworkInterfaceById(1));
+		NetworkInterfaceDAO.deleteNetworkInterface(niSelected);
+		assertNull(NetworkInterfaceDAO.getNetworkInterfaceById(1));
 	}
 	
 	@Test
 	public void TestDevice_CRUD(){
 		Device dev = new Device("Desktop PC", "DELL", "50-1A-C5-F4-C7-BB", "192.168.2.21");
-		DeviceDAO devDAO = new DeviceDAO();
-		devDAO.createDevice(dev);
+		Device newDevice = DeviceDAO.createDevice(dev);
 		
-		Device devSelected = devDAO.getDeviceById(1);
+		Device devSelected = DeviceDAO.getDeviceById(newDevice.getIdDevice());
 		assertEquals("Desktop PC", devSelected.getName());
 		assertEquals("DELL", devSelected.getManufacturer());
 		assertEquals("50-1A-C5-F4-C7-BB", devSelected.getMacAddress());
 		assertEquals("192.168.2.21", devSelected.getIpAddress());
 		
-		//devDAO.deleteDevice(devDAO.getDeviceById(1));
-		//assertNull(devDAO.getDeviceById(1));
+		DeviceDAO.deleteDevice(DeviceDAO.getDeviceById(devSelected.getIdDevice()));
+		assertNull(DeviceDAO.getDeviceById(devSelected.getIdDevice()));
 	}
 	
 	@Test
