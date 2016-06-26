ac5c00d703a06470fcd7214a3347e919a799e74e
ruel85 <ruel.holderegger@outlook.com>
2016-06-26 16:26:43 +0200

    -

--- webapp/src/main/java/ch/zbw/sysVentorySaas/webapp/ScanSettingService.java
+++ webapp/src/main/java/ch/zbw/sysVentorySaas/webapp/ScanSettingService.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriBuilder;
+import javax.xml.ws.Response;
 import javax.xml.ws.ResponseWrapper;
 
 import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.log4j.Logger;
 import org.mockito.internal.stubbing.answers.ThrowsException;
 
 import ch.zbw.sysVentorySaas.App.DataAccessObject.CompanyDAO;
@@ -23,6 +29,9 @@ import ch.zbw.sysVentorySaas.App.model.User;
 
 @Path("/scansettings")
 public class ScanSettingService {
+	
+	static final Logger logger = Logger.getLogger(ScanSettingService.class);
+
 		
 		@GET
 		@Produces(MediaType.APPLICATION_XML)
@@ -40,34 +49,6 @@ public class ScanSettingService {
 			return null;
 		}
 		
-		/*@GET
-		@Path("/{uID}")
-		@Produces(MediaType.APPLICATION_XML)
-		public ScanSetting getScanSettingByUID(@PathParam("uID") String uID)
-		{
-			ScanSetting sReturn = null;
-			System.out.println("UID: " + uID);
-			UserDAO userDAO = new UserDAO();
-			User user = userDAO.getUserByUID(uID);
-			
-			if(user == null)
-				return new ScanSetting("User konnte aufgrund UID (" + uID + ") nicht ermittelt werden!", "", "", 1, false);
-			else if(user.getCompany() == null)
-				return new ScanSetting("Company ist auf User nicht gesetzt!", "", "", 1, false);
-			
-			Company comp = new CompanyDAO().getCompanyById(user.getCompany().getIdCompany());
-			//System.out.println("Company-ID: "  + comp.getIdCompany());
-			//System.out.println("ScanSetting-ID: "  + new ScanSettingDAO().getScanSettingById(comp.getIdCompany()).getIdCompany());
-			
-			try {
-				sReturn = new ScanSettingDAO().getScanSettingById(comp.getIdCompany());	
-			} catch (Exception e) {
-				Log4JLogger log = new Log4JLogger();
-				log.error(e.getMessage());
-			}
-			return sReturn;
-		}*/
-		
 		@GET
 		@Path("/{uID}")
 		@Produces(MediaType.APPLICATION_XML)
@@ -77,26 +58,26 @@ public class ScanSettingService {
 			System.out.println("getScanSettingByUID: " + uID);
 			User user; 			
 			Company comp;
-			
-			try {
-				user = UserDAO.getUserByUID(uID);
-				if(user != null && user.getCompany() != null && user.getCompany().getIdCompany() != 0)
-				{
-					int idCompany = CompanyDAO.getCompanyById(user.getCompany().getIdCompany()).getIdCompany();
-					
-					if(idCompany !=0)
-					{
-						// Todo Ruel
-						//sReturn = ScanSettingDAO.getScanSettingById(idCompany);
-						//return sReturn;
-					}					
-				}
-				
-			} catch (Exception e) {
-				Log4JLogger log = new Log4JLogger();
-				log.error(e.getMessage());
-				throw new ServiceNotFoundException(e.getMessage(), 1);
-			}
+
+			// Todo Ruel
+//			try {
+//				user = UserDAO.getUserByUID(uID);
+//				if(user != null && user.getCompany() != null && user.getCompany().getIdCompany() != 0)
+//				{
+//					int idCompany = CompanyDAO.getCompanyById(user.getCompany().getIdCompany()).getIdCompany();
+//					
+//					if(idCompany !=0)
+//					{
+//						
+//						sReturn = ScanSettingDAO.getScanSettingById(idCompany);
+//						return sReturn;
+//					}					
+//				}
+//				
+//			} catch (Exception e) {
+//				
+//				logger.error(e.getMessage());
+//			}
 			
 			Random r = new Random();
 			int i = r.nextInt(100) + 1;
@@ -105,7 +86,8 @@ public class ScanSettingService {
 			sReturn.setIpStart("192.168.1.30");
 			sReturn.setIpEnd("192.168.1.80");
 			sReturn.setIdCompany(-1);
-			sReturn.setIntervallHours(60);	
+			sReturn.setIntervallHours(60);
+			
 			return sReturn;
 		}
 }
