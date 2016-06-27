aba15ee6ad2c857c8aaa79aa2b7cfff266c03f9e
anonuem <eliasschwarz@elias-schwarzs-macbook-pro.home>
2016-06-27 18:08:48 +0200

    Devices-Seite erstellt und kann aufgerufen werden

--- webapp/src/main/java/ch/zbw/sysVentorySaas/webapp/Beans/Devices.java
+++ webapp/src/main/java/ch/zbw/sysVentorySaas/webapp/Beans/Devices.java
@@ -1,7 +1,6 @@
 package ch.zbw.sysVentorySaas.webapp.Beans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -11,32 +10,30 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
 import ch.zbw.sysVentorySaas.webapp.Util.SessionUtils;
+import ch.zbw.sysVentorySaas.App.DataAccessObject.DeviceDAO;
 import ch.zbw.sysVentorySaas.App.DataAccessObject.ScanJobDAO;
+import ch.zbw.sysVentorySaas.App.model.Device;
 import ch.zbw.sysVentorySaas.App.model.ScanJob;
 
 @ManagedBean
 @SessionScoped
-public class Scans implements Serializable {
+public class Devices implements Serializable {
 
 	private static final long serialVersionUID = 1094801825228386363L;
 
-	private List<ScanJob> scans;
+	private int scanId;
 
-	public String checkScans() {
-		return "Scans";
+	private List<Device> devices;
+
+	public String getDevicesByScanJob(int scanId) {
+		this.scanId = scanId;
+		DeviceDAO devicesDao = new DeviceDAO();
+		this.devices = devicesDao.getDevicesByScanJob(scanId);
+		return "Devices";
 	}
 
-	public List<ScanJob> getScans() {
-		ScanJobDAO scansDao = new ScanJobDAO();
-//		List<ScanJob> allJobs = scansDao.getAllScanJobs();
-//		int userCompany = SessionUtils.getUser().getCompany().getIdCompany();
-//		for (ScanJob job : allJobs) {
-//			//if (job.getIdCompany() == userCompany) {
-//				this.scans.add(job);
-//			//}
-//		}
-		this.scans = scansDao.getAllScanJobs();
-
-		return this.scans;
+	public List<Device> getDevices() {
+		return this.devices;
 	}
+
 }
